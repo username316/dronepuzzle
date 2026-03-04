0bdc531a412c673bc7f86b6ce0171dc16a763400
jtan <46615853+username316@users.noreply.github.com>
2026-03-04 16:43:39 -0500

    Weather
    added fog and rain to levels

--- levels/level.gd
+++ levels/level.gd
@@ -1,16 +1,29 @@
 extends Node3D
+class_name Level
 
 @onready var camera_top = $CameraTop
+@onready var environment = $WorldEnvironment.get_environment()
+@onready var rain_particles = $Rain
 
+#environment
+@export var fog_density: float = 0.0
+@export var raining: bool = false
+#@export var gps_available: bool = true
+
+#cameras
 var current_camera: Camera3D
 
+#controls
 var rotating_camera = false
 
 func _ready() -> void:
 	set_current_camera(camera_top)
+	
+	environment.set_fog_density(fog_density)
+	rain_particles.emitting = raining
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("scroll_up"):
+	'if event.is_action_pressed("scroll_up"):
 		current_camera.zoom_out()
 	if event.is_action_pressed("scroll_down"):
 		current_camera.zoom_in()
@@ -18,7 +31,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("mouse_right"):
 		rotating_camera = true
 	if event.is_action_released("mouse_right"):
-		rotating_camera = false
+		rotating_camera = false'
 		
 	if event is InputEventMouseMotion:
 		if rotating_camera:
