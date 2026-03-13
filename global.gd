a7db2b1d14e541938fad8725e563f6f14caeb3c8
jtan <46615853+username316@users.noreply.github.com>
2026-03-13 17:41:21 -0400

    global level path list
    added Global.level_paths to access a list of all level path strings in the level folder
    level scenes should be named in the format Level_.gd

--- global.gd
+++ global.gd
@@ -1,13 +1,25 @@
 extends Node
 
 var start_menu_path = "res://ui/StartMenu.tscn"
+var levels_folder_path = "res://levels/"
 
 var current_scene = start_menu_path
 
+var level_paths: Array[String]
+
 func _ready() -> void:
 	current_scene =  get_tree().root.get_child(-1)
+	load_levels_from_dir(levels_folder_path)
+	#print(level_paths)
 
-func change_scene_to_path(path):
+func change_scene_to_path(path: String):
 	current_scene.queue_free()
 	current_scene = ResourceLoader.load(path).instantiate()
 	get_tree().root.add_child(current_scene)
+
+func load_levels_from_dir(path: String):
+	var dir = DirAccess.open(path)
+	if dir:
+		for f: String in dir.get_files():
+			if f.begins_with("Level") and f.ends_with(".tscn"):
+				level_paths.append(f)
