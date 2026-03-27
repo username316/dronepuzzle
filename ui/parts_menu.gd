f4eea0fa17e75e69d5b6420f0fd9e42ced301902
vinayakbains <vinayakbains@gmail.com>
2026-03-27 01:32:54 -0400

    Put new variables in the basic parts menu

--- ui/parts_menu.gd
+++ ui/parts_menu.gd
@@ -2,11 +2,11 @@ extends Control
 
 var vision: bool = false
 var lidar: bool = false
-var minThrust: float = 0.0
-var maxThrust: float = 60.0
-var brakeStrength: float = 8.0
-var lidarStopRange: float = 9.0
-var tiltSpeed: float = 3.0
+var speed: float = 0.5
+var handling: float = 0.5
+var caution: float = 0.5
+var climbBias: float = 0.5
+var precision: float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,28 +30,28 @@ func _on_lidar_toggled(toggled_on: bool) -> void:
 	lidar = toggled_on
 
 
-func _on_min_thrust_changed(value: float) -> void:
-	minThrust = value
+func _on_speed_changed(value: float) -> void:
+	speed = value
 	$Label5.text = str(value)
 
 
-func _on_max_thrust_value_changed(value: float) -> void:
-	maxThrust = value
+func _on_handling_changed(value: float) -> void:
+	handling = value
 	$Label7.text = str(value)
 
 
-func _on_brake_strength_changed(value: float) -> void:
-	brakeStrength = value
+func _on_caution_changed(value: float) -> void:
+	caution = value
 	$Label9.text = str(value)
 
 
-func _on_lidar_stop_range_changed(value: float) -> void:
-	lidarStopRange = value
+func _on_climb_bias_changed(value: float) -> void:
+	climbBias = value
 	$Label11.text = str(value)
 
 
-func _on_tilt_speed_changed(value: float) -> void:
-	tiltSpeed = value
+func _on_precision_changed(value: float) -> void:
+	precision = value
 	$Label13.text = str(value)
 
 func _on_exit_pressed() -> void:
@@ -61,11 +61,11 @@ func _on_exit_pressed() -> void:
 func _on_next_pressed() -> void:
 	Global.vision_enabled = vision
 	Global.lidar_enabled = lidar
-	Global.minThrust = minThrust
-	Global.maxThrust = maxThrust
-	Global.brakeStrength = brakeStrength
-	Global.lidarStopRange = lidarStopRange
-	Global.tiltSpeed = tiltSpeed
+	Global.speed = speed
+	Global.handling = handling
+	Global.caution = caution
+	Global.climbBias = climbBias
+	Global.precision = precision
 	
 	Global.change_scene_to_path("res://ui/AdvancedPartsMenu.tscn")
 
@@ -73,19 +73,19 @@ func _on_next_pressed() -> void:
 func _on_reset_pressed() -> void:
 	vision = false
 	lidar = false
-	minThrust = 0.0
-	maxThrust = 60.0
-	brakeStrength = 8.0
-	lidarStopRange = 9.0
-	tiltSpeed = 3.0
+	speed = 0.5
+	handling = 0.5
+	caution = 0.5
+	climbBias = 0.5
+	precision = 0.5
 	
 	$CheckBox.button_pressed = vision
 	$CheckBox2.button_pressed = lidar
-	$HSlider.value = minThrust
-	$HSlider2.value = maxThrust
-	$HSlider3.value = brakeStrength
-	$HSlider4.value = lidarStopRange
-	$HSlider5.value = tiltSpeed
+	$HSlider.value = speed
+	$HSlider2.value = handling
+	$HSlider3.value = caution
+	$HSlider4.value = climbBias
+	$HSlider5.value = precision
 	
 	$Label5.text = str($HSlider.value)
 	$Label7.text = str($HSlider2.value)
